2b14c546f4938506cfeea7deea467586bf10e2a2
rosa <fernando.rosag11@gmail.com>
2024-02-15 11:13:19 -0300

    APP - Create Drawer and avatar circle.

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,28 +9,27 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    var _selectedIndex = 0;
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu),
-        ),
         toolbarHeight: 100,
         backgroundColor: Colors.transparent,
         actions: [
           Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(16.0),
             child: ClipOval(
               child: Material(
-                color: Colors.blue,
+                color: Colors.transparent,
                 child: InkWell(
                   splashColor: Colors.red, // Splash color
                   onTap: () {},
                   child: const SizedBox(
                     width: 70,
                     height: 70,
-                    child: Icon(Icons.person),
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          "https://pipocamusical.files.wordpress.com/2011/06/sucker-punch-sucker-punch-19983470-1920-1080.jpg"),
+                    ),
                   ),
                 ),
               ),
@@ -67,6 +66,72 @@ class HomeView extends GetView<HomeController> {
             label: 'Chats',
           ),
         ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            Stack(
+              children: <Widget>[
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.transparent,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          "https://pipocamusical.files.wordpress.com/2011/06/sucker-punch-sucker-punch-19983470-1920-1080.jpg"),
+                    ),
+                  ),
+                  height: 175.0,
+                ),
+                Container(
+                  height: 175.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    gradient: LinearGradient(
+                      begin: FractionalOffset.topCenter,
+                      end: FractionalOffset.bottomCenter,
+                      colors: [
+                        Colors.grey.withOpacity(0.0),
+                        Colors.black,
+                      ],
+                      stops: const [0.0, 1.0],
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.bottomLeft,
+                  height: 175.0,
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Text(
+                      "Welcome Babydoll!",
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ListTile(
+              leading: Icon(Icons.home_outlined),
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.task_alt_outlined),
+              title: const Text('Tasks'),
+              selected: _selectedIndex == 1,
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.summarize_outlined),
+              title: const Text('Summary'),
+              selected: _selectedIndex == 2,
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
